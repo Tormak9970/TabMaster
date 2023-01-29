f862b5a5636af807d4515eba21577fa426f11295
Witherking25 <witherking@withertech.com>
2023-01-29 18:24:26 -0500

    fix things

--- main.py
+++ main.py
@@ -5,89 +5,90 @@ from typing import TypeVar, Dict, List
 from settings import SettingsManager
 
 logging.basicConfig(filename="/tmp/tabmaster.log",
-                    format='[TabMaster] %(asctime)s %(levelname)s %(message)s',
-                    filemode='w+',
-                    force=True)
+					format='[TabMaster] %(asctime)s %(levelname)s %(message)s',
+					filemode='w+',
+					force=True)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)  # can be changed to logging.DEBUG for debugging issues
 
 
 class Plugin:
-    tabs: Dict[str, dict] = None
-    hidden_tabs: List[str] = None
-    settings: SettingsManager
+	tabs: Dict[str, dict] = None
+	hidden_tabs: List[str] = None
+	settings: SettingsManager
 
-    async def _main(self):
-        Plugin.settings = SettingsManager("tabmaster")
-        await Plugin.read(self)
-        Plugin.tabs = await Plugin.get_setting(self, "tabs", {})
-        Plugin.hidden_tabs = await Plugin.get_setting(self, "hidden_tabs", [])
+	async def _main(self):
+		Plugin.settings = SettingsManager("tabmaster")
+		await Plugin.read(self)
 
-    async def _unload(self):
-        await Plugin.set_setting(self, "tabs", Plugin.tabs)
-        await Plugin.set_setting(self, "hidden_tabs", Plugin.hidden_tabs)
+	async def _unload(self):
+		await Plugin.commit(self)
 
-    async def get_tabs(self) -> Dict[str, dict] | None:
-        """
-        Waits until tabs is loaded, then returns the tabs
+	async def get_tabs(self) -> Dict[str, dict] | None:
+		"""
+		Waits until tabs is loaded, then returns the tabs
 
-        :return: The tabs
-        """
-        while Plugin.tabs is None:
-            await asyncio.sleep(0.1)
-        logger.debug(f"Got tabs {Plugin.tabs}")
-        return Plugin.tabs
+		:return: The tabs
+		"""
+		while Plugin.tabs is None:
+			await asyncio.sleep(0.1)
+		logger.debug(f"Got tabs {Plugin.tabs}")
+		return Plugin.tabs
 
-    async def set_tabs(self, tabs: Dict[str, dict]):
-        Plugin.tabs = tabs
-        await Plugin.set_setting(self, "tabs", Plugin.tabs)
+	async def set_tabs(self, tabs: Dict[str, dict]):
+		Plugin.tabs = tabs
+		await Plugin.set_setting(self, "tabs", Plugin.tabs)
 
-    async def get_hidden_tabs(self) -> List[str] | None:
-        """
-        Waits until tabs is loaded, then returns the tabs
+	async def get_hidden_tabs(self) -> List[str] | None:
+		"""
+		Waits until tabs is loaded, then returns the tabs
 
-        :return: The tabs
-        """
-        while Plugin.hidden_tabs is None:
-            await asyncio.sleep(0.1)
-        logger.debug(f"Got hidden tabs {Plugin.hidden_tabs}")
-        return Plugin.hidden_tabs
+		:return: The tabs
+		"""
+		while Plugin.hidden_tabs is None:
+			await asyncio.sleep(0.1)
+		logger.debug(f"Got hidden tabs {Plugin.hidden_tabs}")
+		return Plugin.hidden_tabs
 
-    async def set_hidden_tabs(self, tabs: List[str]):
-        Plugin.hidden_tabs = tabs
-        await Plugin.set_setting(self, "hidden_tabs", Plugin.hidden_tabs)
+	async def set_hidden_tabs(self, tabs: List[str]):
+		Plugin.hidden_tabs = tabs
+		await Plugin.set_setting(self, "hidden_tabs", Plugin.hidden_tabs)
 
-    async def read(self) -> None:
-        """
-        Reads the json from disk
-        """
-        Plugin.settings.read()
+	async def read(self) -> None:
+		"""
+		Reads the json from disk
+		"""
+		Plugin.settings.read()
+		Plugin.tabs = await Plugin.get_setting(self, "tabs", {})
+		Plugin.hidden_tabs = await Plugin.get_setting(self, "hidden_tabs", [])
 
-    async def commit(self) -> None:
-        """
-        Commits the json to disk
-        """
-        Plugin.settings.commit()
+	async def commit(self) -> None:
+		"""
+		Commits the json to disk
+		"""
+		Plugin.settings.commit()
+		await Plugin.set_setting(self, "tabs", Plugin.tabs)
+		await Plugin.set_setting(self, "hidden_tabs", Plugin.hidden_tabs)
 
-    T = TypeVar("T")
+	T = TypeVar("T")
 
-    async def get_setting(self, key, default: T) -> T:
-        """
-        Gets the specified setting from the json
+	async def get_setting(self, key, default: T) -> T:
+		"""
+		Gets the specified setting from the json
 
-        :param key: The key to get
-        :param default: The default value
-        :return: The value, or default if not found
-        """
-        return Plugin.settings.getSetting(key, default)
+		:param key: The key to get
+		:param default: The default value
+		:return: The value, or default if not found
+		"""
+		return Plugin.settings.getSetting(key, default)
 
-    async def set_setting(self, key, value: T) -> T:
-        """
-        Sets the specified setting in the json
+	async def set_setting(self, key, value: T) -> T:
+		"""
+		Sets the specified setting in the json
 
-        :param key: The key to set
-        :param value: The value to set it to
-        :return: The new value
-        """
-        Plugin.settings.setSetting(key, value)
-        return value
+		:param key: The key to set
+		:param value: The value to set it to
+		:return: The new value
+		"""
+		Plugin.settings.setSetting(key, value)
+		return value
--- src/AppPatch.tsx
+++ src/AppPatch.tsx
@@ -1,4 +1,10 @@
-import {afterPatch, RoutePatch, ServerAPI, wrapReactType} from "decky-frontend-lib";
+import {
+	afterPatch,
+	fakeRenderComponent,
+	RoutePatch,
+	ServerAPI,
+	wrapReactType
+} from "decky-frontend-lib";
 import {JSXElementConstructor, ReactElement} from "react";
 import {SteamCollection, SteamTab} from "./SteamTypes";
 import {cloneDeep} from "lodash";
@@ -6,6 +12,15 @@ import {customTabs, tabOrder, tabsToHide} from "./TabMasterState";
 
 export const patchAppPage = (serverAPI: ServerAPI): RoutePatch =>
 {
+	console.log((() =>
+	{
+		// @ts-ignore
+		let ret1 = window.DeckyPluginLoader.routerHook.routes
+				.find((x: any) => x.props.path=='/library')
+				.props.children.type()
+		wrapReactType(ret1.type)
+		return fakeRenderComponent(ret1.type)
+	}))
 	let TabContentTemplate: JSXElementConstructor<{ collection: SteamCollection, eSortBy: number, setSortBy: (e: any) => void, showSortingContextMenu: (e: any) => void }>;
 	let TabContentPropsTemplate: { collection: SteamCollection, eSortBy: number, setSortBy: (e: any) => void, showSortingContextMenu: (e: any) => void };
 	let TabAddonTemplate: JSXElementConstructor<{ count: number }>;
@@ -16,11 +31,11 @@ export const patchAppPage = (serverAPI: ServerAPI): RoutePatch =>
 		{
 			let cache: any;
 			let currentTab: string;
-			console.log("ret1", ret1);
+			// console.log("ret1", ret1);
 			wrapReactType(ret1.type);
 			afterPatch(ret1, "type", (_: Record<string, unknown>[], ret2: ReactElement) =>
 			{
-				console.log("ret2", ret2);
+				// console.log("ret2", ret2);
 				currentTab = ret2.props.tab
 				// @ts-ignore
 				wrapReactType(ret2.type.type);
@@ -30,7 +45,29 @@ export const patchAppPage = (serverAPI: ServerAPI): RoutePatch =>
 				})
 				afterPatch(ret2.type, "type", (_: Record<string, unknown>[], ret3: ReactElement) =>
 				{
-					console.log("ret3", ret3);
+					// console.log("ret3", ret3);
+					if (ret3.props.className === "gamepadlibrary_GamepadLibrary_ZBBhe")
+						ret3.props.children.props.isLibrary = true
+					if (TabContentTemplate===undefined || TabAddonTemplate===undefined)
+					{
+						let tabs = (ret3.props.children.props.tabs as SteamTab[]).filter(value => value!==undefined) as SteamTab[]
+						let tabTemplate = tabs.find(value => value !== undefined && value?.id==="Favorites");
+						// console.log("tabTemplate", tabTemplate)
+						if (tabTemplate)
+						{
+							const tabContent = (tabTemplate.content.type)
+							TabContentPropsTemplate = (tabTemplate.content.props)
+							const tabAddon = (tabTemplate.renderTabAddon().type)
+							if (typeof tabContent!=="string" && tabContent!==undefined)
+							{
+								TabContentTemplate = tabContent;
+							}
+							if (typeof tabAddon!=="string" && tabAddon!==undefined)
+							{
+								TabAddonTemplate = tabAddon;
+							}
+						}
+					}
 					wrapReactType(ret3.props.children.type.type)
 					if (cache)
 					{
@@ -39,28 +76,10 @@ export const patchAppPage = (serverAPI: ServerAPI): RoutePatch =>
 					{
 						afterPatch(ret3.props.children.type, "type", (_: Record<string, any>[], ret4: ReactElement) =>
 						{
-							if (ret4.props.autoFocusContents)
+							if (ret4.props?.isLibrary)
 							{
-								console.log("ret4", ret4);
+								// console.log("ret4", ret4);
 								let tabs = (ret4.props.tabs as SteamTab[]).filter(value => value!==undefined) as SteamTab[];
-								if (TabContentTemplate===undefined || TabAddonTemplate===undefined)
-								{
-									let tabTemplate = tabs.find(value => value.id=="Favorites");
-									if (tabTemplate)
-									{
-										const tabContent = tabTemplate.content.type
-										TabContentPropsTemplate = tabTemplate.content.props
-										const tabAddon = tabTemplate.renderTabAddon().type
-										if (typeof tabContent!=="string" && tabContent!==undefined)
-										{
-											TabContentTemplate = tabContent;
-										}
-										if (typeof tabAddon!=="string" && tabAddon!==undefined)
-										{
-											TabAddonTemplate = tabAddon;
-										}
-									}
-								}
 								tabs = tabs.filter(value => !tabsToHide.includes(value.id));
 								if (tabsToHide.includes(ret4.props.activeTab))
 								{
@@ -71,24 +90,27 @@ export const patchAppPage = (serverAPI: ServerAPI): RoutePatch =>
 								ret4.props.activeTab = currentTab;
 								customTabs.forEach((value, key) =>
 								{
-
-									const Collection: SteamCollection = cloneDeep(collectionStore.allAppsCollection);
-
-									Collection.allApps = cloneDeep(collectionStore.allAppsCollection.allApps).filter(app => value.filters.every(filter => filter.filter(app) && app.app_type!==4))
-									Collection.visibleApps = cloneDeep(collectionStore.allAppsCollection.visibleApps).filter(app => value.filters.every(filter => filter.filter(app) && app.app_type!==4))
-									const NewTab: SteamTab = {
-										id: key,
-										title: value.title,
-										content: <TabContentTemplate collection={Collection}
-										                             eSortBy={TabContentPropsTemplate.eSortBy}
-										                             setSortBy={TabContentPropsTemplate.setSortBy}
-										                             showSortingContextMenu={TabContentPropsTemplate.showSortingContextMenu}/>,
-										renderTabAddon: () => <TabAddonTemplate count={Collection.visibleApps.length}/>,
-									}
-
-									if (tabs.every(value => value.id!=NewTab.id))
+									if (TabContentTemplate !== undefined && TabContentPropsTemplate !== undefined && TabAddonTemplate !== undefined)
 									{
-										tabs.push(NewTab);
+										const Collection: SteamCollection = cloneDeep(collectionStore.allAppsCollection);
+
+										Collection.allApps = collectionStore.allAppsCollection.allApps.filter(app => value.filters.every(filter => filter.filter(app) && app.app_type!==4))
+										Collection.visibleApps = collectionStore.allAppsCollection.visibleApps.filter(app => value.filters.every(filter => filter.filter(app) && app.app_type!==4))
+										const NewTab: SteamTab = {
+											id: key,
+											title: value.title,
+											content: <TabContentTemplate collection={Collection}
+											                             eSortBy={TabContentPropsTemplate.eSortBy ?? 0}
+											                             setSortBy={TabContentPropsTemplate.setSortBy ?? (() => {})}
+											                             showSortingContextMenu={TabContentPropsTemplate.showSortingContextMenu ?? (() => {})}/>,
+											renderTabAddon: () => <TabAddonTemplate
+													count={Collection.visibleApps.length}/>,
+										}
+
+										if (tabs.every(value => value.id!=NewTab.id))
+										{
+											tabs.push(NewTab);
+										}
 									}
 								});
 
