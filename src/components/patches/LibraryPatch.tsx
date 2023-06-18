066bd252e26c2c10815b713fe74a5c1c89802fb5
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-06-18 14:25:33 -0500

    fix: drastically improved patching performance

--- src/components/patches/LibraryPatch.tsx
+++ src/components/patches/LibraryPatch.tsx
@@ -36,94 +36,99 @@ export const patchLibrary = (serverAPI: ServerAPI): RoutePatch => {
 					return ret;
 				});
 
-        //* This runs 129 times, which can 100% be improved
-				afterPatch(ret2.type, "type", (_: Record<string, unknown>[], ret3: ReactElement) => {
-					console.log("ret3", ret3);
+        if (outerCache) {
+          ret2.type = outerCache;
+        } else {
+          //* This runs 129 times, which can 100% be improved
+          afterPatch(ret2.type, "type", (_: Record<string, unknown>[], ret3: ReactElement) => {
+            console.log("ret3", ret3);
 
-					let element = findInReactTree(ret3, (x) => x?.props?.tabs);
-					console.log("className: ", findInReactTree(ret3, (x) => x?.props?.className), findInReactTree(ret3, (x) => x?.props?.className).props.className);
+            let element = findInReactTree(ret3, (x) => x?.props?.tabs);
+            console.log("className: ", findInReactTree(ret3, (x) => x?.props?.className), findInReactTree(ret3, (x) => x?.props?.className).props.className);
 
-					if (findInReactTree(ret3, (x) => x?.props?.className?.includes("gamepadlibrary_GamepadLibrary") && !x?.props?.className?.includes(" "))) {
-            element.props.isLibrary = true;
-          }
+            if (findInReactTree(ret3, (x) => x?.props?.className?.includes("gamepadlibrary_GamepadLibrary") && !x?.props?.className?.includes(" "))) {
+              element.props.isLibrary = true;
+            }
 
-					console.log("isLibrary: ", !!element.props?.isLibrary);
+            console.log("isLibrary: ", !!element.props?.isLibrary);
 
-					if (TabContentTemplate===undefined || TabAddonTemplate===undefined) {
-						let tabs = (element.props.tabs as SteamTab[]).filter((value) => value !== undefined) as SteamTab[];
-						let tabTemplate = tabs.find((value) => value !== undefined && value?.id === "Favorites");
+            if (TabContentTemplate===undefined || TabAddonTemplate===undefined) {
+              let tabs = (element.props.tabs as SteamTab[]).filter((value) => value !== undefined) as SteamTab[];
+              let tabTemplate = tabs.find((value) => value !== undefined && value?.id === "Favorites");
 
-						console.log("tabTemplate", tabTemplate)
+              console.log("tabTemplate", tabTemplate)
 
-						if (tabTemplate) {
-							const tabContent = (tabTemplate.content.type);
-							TabContentPropsTemplate = (tabTemplate.content.props);
-							const tabAddon = (tabTemplate.renderTabAddon().type);
+              if (tabTemplate) {
+                const tabContent = (tabTemplate.content.type);
+                TabContentPropsTemplate = (tabTemplate.content.props);
+                const tabAddon = (tabTemplate.renderTabAddon().type);
 
-							if (typeof tabContent !== "string" && tabContent !== undefined) {
-								TabContentTemplate = tabContent;
-							}
+                if (typeof tabContent !== "string" && tabContent !== undefined) {
+                  TabContentTemplate = tabContent;
+                }
 
-							if (typeof tabAddon !== "string" && tabAddon !== undefined) {
-								TabAddonTemplate = tabAddon;
-							}
-						}
-					}
+                if (typeof tabAddon !== "string" && tabAddon !== undefined) {
+                  TabAddonTemplate = tabAddon;
+                }
+              }
+            }
 
-					wrapReactType(element.type.type);
-					if (tabsCache) {
-						element.type = tabsCache;
-					} else {
-						afterPatch(element.type, "type", (_: Record<string, any>[], ret4: ReactElement) => {
-							if (ret4.props?.isLibrary) {
-								console.log("ret4", ret4);
+            wrapReactType(element.type.type);
+            if (tabsCache) {
+              element.type = tabsCache;
+            } else {
+              afterPatch(element.type, "type", (_: Record<string, any>[], ret4: ReactElement) => {
+                if (ret4.props?.isLibrary) {
+                  console.log("ret4", ret4);
 
-								let tabs = (ret4.props.tabs as SteamTab[]).filter(value => value!==undefined) as SteamTab[];
-								tabs = tabs.filter(value => !tabsToHide.includes(value.id));
-								
-                if (tabsToHide.includes(ret4.props.activeTab)) {
-									if (tabs[0]!==undefined) ret4.props.activeTab = tabs[0].id;
-								}
-                
-								console.log("tabs: ", tabs);
-								ret4.props.activeTab = currentTab;
+                  let tabs = (ret4.props.tabs as SteamTab[]).filter(value => value!==undefined) as SteamTab[];
+                  tabs = tabs.filter(value => !tabsToHide.includes(value.id));
+                  
+                  if (tabsToHide.includes(ret4.props.activeTab)) {
+                    if (tabs[0]!==undefined) ret4.props.activeTab = tabs[0].id;
+                  }
+                  
+                  console.log("tabs: ", tabs);
+                  ret4.props.activeTab = currentTab;
 
-								customTabs.forEach((value, key) => {
-									if (TabContentTemplate !== undefined && TabContentPropsTemplate !== undefined && TabAddonTemplate !== undefined) {
-										const collection: SteamCollection = cloneDeep(collectionStore.allAppsCollection);
+                  customTabs.forEach((value, key) => {
+                    if (TabContentTemplate !== undefined && TabContentPropsTemplate !== undefined && TabAddonTemplate !== undefined) {
+                      const collection: SteamCollection = cloneDeep(collectionStore.allAppsCollection);
 
-										collection.allApps = collectionStore.allAppsCollection.allApps.filter(app => value.filters.every(filter => filter.filter(app) && app.app_type!==4))
-										collection.visibleApps = collectionStore.allAppsCollection.visibleApps.filter(app => value.filters.every(filter => filter.filter(app) && app.app_type!==4))
-										const newTab: SteamTab = {
-											id: key,
-											title: value.title,
-											content: <TabContentTemplate
-                        collection={collection}
-                        eSortBy={TabContentPropsTemplate.eSortBy ?? 0}
-                        setSortBy={TabContentPropsTemplate.setSortBy ?? (() => {})}
-                        showSortingContextMenu={TabContentPropsTemplate.showSortingContextMenu ?? (() => {})}/>,
-											renderTabAddon: () => <TabAddonTemplate count={collection.visibleApps.length}/>,
-										}
+                      collection.allApps = collectionStore.allAppsCollection.allApps.filter(app => value.filters.every(filter => filter.filter(app) && app.app_type!==4))
+                      collection.visibleApps = collectionStore.allAppsCollection.visibleApps.filter(app => value.filters.every(filter => filter.filter(app) && app.app_type!==4))
+                      const newTab: SteamTab = {
+                        id: key,
+                        title: value.title,
+                        content: <TabContentTemplate
+                          collection={collection}
+                          eSortBy={TabContentPropsTemplate.eSortBy ?? 0}
+                          setSortBy={TabContentPropsTemplate.setSortBy ?? (() => {})}
+                          showSortingContextMenu={TabContentPropsTemplate.showSortingContextMenu ?? (() => {})}/>,
+                        renderTabAddon: () => <TabAddonTemplate count={collection.visibleApps.length}/>,
+                      }
 
-										if (tabs.every(value => value.id! = newTab.id)) {
-											tabs.push(newTab);
-										}
-									}
-								});
+                      if (tabs.every(value => value.id! = newTab.id)) {
+                        tabs.push(newTab);
+                      }
+                    }
+                  });
 
-								console.log("customTabs: ", tabs);
-								ret4.props.tabs = tabs.sort((a, b) => (tabOrder.get(a.id) ?? 0) - (tabOrder.get(b.id) ?? 0));
-							} else {
-								ret4.props.tabs = (ret4.props.tabs as SteamTab[]).filter(value => !Array.from(customTabs.keys()).includes(value.id));
-							}
-							console.log(element);
+                  console.log("customTabs: ", tabs);
+                  ret4.props.tabs = tabs.sort((a, b) => (tabOrder.get(a.id) ?? 0) - (tabOrder.get(b.id) ?? 0));
+                } else {
+                  ret4.props.tabs = (ret4.props.tabs as SteamTab[]).filter(value => !Array.from(customTabs.keys()).includes(value.id));
+                }
+                console.log(element);
 
-							return ret4;
-						});
-						tabsCache = element.type;
-					}
-					return ret3;
-				});
+                return ret4;
+              });
+              tabsCache = element.type;
+            }
+            return ret3;
+          });
+          outerCache = ret2.type;
+        }
 				return ret2;
 			});
 			return ret1;
