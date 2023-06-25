e19503cdc8377874ebd9e9a47873f142fd8ea728
Jesse Bofill <jesse_bofill@yahoo.com>
2023-06-24 23:47:01 -0600

    chore: remove unused backed methods

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ Initialized = False
 
 class Plugin:
   tabs: Dict[str, dict] = None
-  hidden_tabs: List[str] = None
   settings: SettingsManager
 
   async def logMessage(self, message, level):
@@ -44,28 +43,12 @@ class Plugin:
     Plugin.tabs = tabs
     await Plugin.set_setting(self, "tabs", Plugin.tabs)
 
-  async def get_hidden_tabs(self) -> List[str] | None:
-    """
-    Waits until tabs is loaded, then returns the tabs
-
-    :return: The tabs
-    """
-    while Plugin.hidden_tabs is None:
-      await asyncio.sleep(0.1)
-    log(f"Got hidden tabs {Plugin.hidden_tabs}")
-    return Plugin.hidden_tabs
-
-  async def set_hidden_tabs(self, tabs: List[str]):
-    Plugin.hidden_tabs = tabs
-    await Plugin.set_setting(self, "hidden_tabs", Plugin.hidden_tabs)
-
   async def read(self) -> None:
     """
     Reads the json from disk
     """
     Plugin.settings.read()
     Plugin.tabs = await Plugin.get_setting(self, "tabs", {})
-    Plugin.hidden_tabs = await Plugin.get_setting(self, "hidden_tabs", [])
 
   async def commit(self) -> None:
     """
@@ -73,7 +56,6 @@ class Plugin:
     """
     Plugin.settings.commit()
     await Plugin.set_setting(self, "tabs", Plugin.tabs)
-    await Plugin.set_setting(self, "hidden_tabs", Plugin.hidden_tabs)
 
   T = TypeVar("T")
 
