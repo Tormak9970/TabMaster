70eeaf74e0fba4057ac934647f21c0a5259b0f5e
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-06-28 16:29:34 -0500

    chore: removed ctx patching

--- src/index.tsx
+++ src/index.tsx
@@ -127,7 +127,7 @@ export default definePlugin((serverAPI: ServerAPI) => {
   const loginUnregisterer = PluginController.initOnLogin(async () => {
     await tabMasterManager.loadTabs();
     libraryPatch = patchLibrary(serverAPI, tabMasterManager);
-    settingsPatch = patchSettings(serverAPI,tabMasterManager)
+    settingsPatch = patchSettings(serverAPI, tabMasterManager);
   });
 
   return {
