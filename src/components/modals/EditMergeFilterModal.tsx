762912ad1032830343aede0bc2673c3323064e03
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-07-05 14:09:54 -0500

    chore: fixed typo in build

--- src/components/modals/EditMergeFilterModal.tsx
+++ src/components/modals/EditMergeFilterModal.tsx
@@ -1,7 +1,7 @@
 import { ConfirmModal } from "decky-frontend-lib"
 import { VFC, useState, Fragment, useEffect } from "react"
 import { ModalStyles } from "../styles/ModalStyles"
-import { FilterEditorPanel } from "../filters/FilterEditorPanel"
+import { FiltersPanel } from "../filters/FiltersPanel"
 import { TabFilterSettings, FilterType } from "../filters/Filters"
 import { isDefaultParams } from "../filters/Filters"
 import { PythonInterop } from "../../lib/controllers/PythonInterop"
@@ -33,7 +33,7 @@ export const EditMergeFilterModal: VFC<EditMergeFilterModalProps> = ({ closeModa
     const updatedFilters = [...groupFilters];
     updatedFilters.push({
       type: "collection",
-      params: { collection: "" }
+      params: { id: "", name: "" }
     });
 
     setGroupFilters(updatedFilters);
@@ -58,7 +58,7 @@ export const EditMergeFilterModal: VFC<EditMergeFilterModalProps> = ({ closeModa
       <ModalStyles />
       <div className="tab-master-modal-scope">
         <ConfirmModal onOK={onOkButton} onCancel={closeModal} strTitle="Union Group">
-          <FilterEditorPanel
+          <FiltersPanel
             groupFilters={groupFilters}
             setGroupFilters={setGroupFilters}
             addFilter={addFilterToGroup}
