cb4a911304cebc6a4b87104fb4844b05e79e79d3
Jesse Bofill <jesse_bofill@yahoo.com>
2023-07-06 01:05:32 -0600

    chore: progress towards deleting tabs and filters

--- src/components/accordions/TabErrorsAccordion.tsx
+++ src/components/accordions/TabErrorsAccordion.tsx
@@ -1,20 +1,21 @@
-import { Button, Focusable } from "decky-frontend-lib"
-import React, { VFC, useState } from "react"
-import { BiSolidDownArrow } from "react-icons/bi"
-import { FaCircleCheck, FaCircleExclamation } from "react-icons/fa6"
+import { Button, Focusable } from "decky-frontend-lib";
+import React, { VFC, useState, Fragment } from "react";
+import { BiSolidDownArrow } from "react-icons/bi";
+import { FaCircleCheck, FaCircleExclamation, FaCircleXmark } from "react-icons/fa6";
 
 type TabErrorsAccordionProps = {
   index: number,
   tab: TabSettings,
   isPassing: boolean,
+  isDeleted: boolean,
   isOpen: boolean,
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 /**
  * Filter Section accordion component
  */
-export const TabErrorsAccordion: VFC<TabErrorsAccordionProps> = ({ index, isPassing, tab, isOpen, children }) => {
+export const TabErrorsAccordion: VFC<TabErrorsAccordionProps> = ({ index, isPassing, isDeleted, tab, isOpen, children }) => {
   const [open, setOpen] = useState(isOpen);
 
   function onClick(e: any) {
@@ -38,27 +39,35 @@ export const TabErrorsAccordion: VFC<TabErrorsAccordionProps> = ({ index, isPass
           justifyContent: "space-between",
           alignItems: "center"
         }} onOKButton={onClick} onClick={onClick}>
-          <div className="check-cont">
-            {isPassing ? (
-              // TODO: better green
-              <FaCircleCheck fill="green" />
-            ) : (
-              // TODO: better yellow
-              <FaCircleExclamation fill="yellow" />
-            )}
-            Tab {index + 1} - {tab.title}
-          </div>
-          <BiSolidDownArrow
-            style={{
-              animation: "transform 0.2s ease-in-out",
-              transform: !open ? "rotate(90deg)" : "",
-              fontSize: "0.8em",
-              marginLeft: "5px"
-            }}
-          />
+          {isDeleted ?
+            <div className="check-cont">
+              <FaCircleXmark fill="red" />
+              Deleting Tab {index + 1} - {tab.title}
+            </div> :
+            <>
+              <div className="check-cont">
+                {isPassing ? (
+                  // TODO: better green
+                  <FaCircleCheck fill="green" />
+                ) : (
+                  // TODO: better yellow
+                  <FaCircleExclamation fill="yellow" />
+                )}
+                Tab {index + 1} - {tab.title}
+              </div>
+              <BiSolidDownArrow
+                style={{
+                  animation: "transform 0.2s ease-in-out",
+                  transform: !open ? "rotate(90deg)" : "",
+                  fontSize: "0.8em",
+                  marginLeft: "5px"
+                }}
+              />
+            </>
+          }
         </Button>
       </Focusable>
-      {open && children}
+      {open && !isDeleted && children}
     </Focusable>
-  )
-}
+  );
+};
--- src/components/changes-needed/TabErrorsPanel.tsx
+++ src/components/changes-needed/TabErrorsPanel.tsx
@@ -1,14 +1,16 @@
-import { VFC, useState } from "react"
-import { TabErrorsAccordion } from "../accordions/TabErrorsAccordion"
-import { FilterErrorOptions } from "./FilterErrorOptions"
-import { FilterType, TabFilterSettings } from "../filters/Filters"
+import { VFC, useState } from "react";
+import { TabErrorsAccordion } from "../accordions/TabErrorsAccordion";
+import { FilterErrorOptions } from "./FilterErrorOptions";
+import { FilterType, TabFilterSettings } from "../filters/Filters";
+import { ButtonItem } from "decky-frontend-lib";
+import { LogController } from "../../lib/controllers/LogController";
 
 type TabErrorsPanelProps = {
   index: number,
   tab: TabSettings,
   erroredFilters: FilterErrorEntry[],
-  onTabStatusChange: (tab: TabSettings, isPassing: boolean) => void
-}
+  onTabStatusChange: (tab: TabSettings, isPassing: boolean) => void;
+};
 
 /**
  * Panel for tab that needs changes
@@ -16,6 +18,8 @@ type TabErrorsPanelProps = {
 export const TabErrorsPanel: VFC<TabErrorsPanelProps> = ({ index, tab, erroredFilters, onTabStatusChange }) => {
   const [errorMessages, setErrorMessages] = useState<string[][]>(erroredFilters.map((entry: FilterErrorEntry) => entry.errors));
   const [isPassing, setIsPassing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
 
   function handleFilterUpdate(filterListIdx: number, errorMessageIdx: number, filter: TabFilterSettings<FilterType>) {
     const messages = [...errorMessages];
@@ -31,23 +35,45 @@ export const TabErrorsPanel: VFC<TabErrorsPanelProps> = ({ index, tab, erroredFi
     onTabStatusChange(tab, passing);
   }
 
+  //*this does not work, indexes will get off
+  function deleteFilter(filterListIdx: number, errorMessageIdx: number) {
+    const messages = [...errorMessages];
+    messages[errorMessageIdx] = [];
+    setErrorMessages(messages);
+
+    const filters = [...tab.filters!];
+    filters.splice(filterListIdx, 1);
+    tab.filters = filters;
+
+    const passing = messages.every((entry) => entry.length === 0);
+    setIsPassing(passing);
+    setIsDeleting(filters.length === 0);
+    onTabStatusChange(tab, passing);
+  }
+
   return (
-    <TabErrorsAccordion index={index} tab={tab} isPassing={isPassing} isOpen={true}>
+    <TabErrorsAccordion index={index} tab={tab} isPassing={isPassing} isOpen={true} isDeleted={isDeleting}>
       {erroredFilters.map((erroredFilter: FilterErrorEntry, errorIdx: number) => {
         const filter = tab.filters![erroredFilter.filterIdx];
 
         return (
           <div className="filter-error-entry">
             <div className="filter-error-messages">
-              {errorMessages[errorIdx].map((errorMsg:string, idx: number) => (
+              {errorMessages[errorIdx].map((errorMsg: string, idx: number) => (
                 <div className="filter-error-msg">Error {idx + 1} - {errorMsg}</div>
               ))}
             </div>
             <div className="filter-type">Filter Type - {filter.type}</div>
             <FilterErrorOptions filter={filter} onFilterUpdate={(filter) => handleFilterUpdate(erroredFilter.filterIdx, errorIdx, filter)} />
+            //* probably should be trash icon next to filter options, and show modal
+            <ButtonItem onClick={() => {
+              deleteFilter(erroredFilter.filterIdx, errorIdx);
+            }} >
+              Delete Filter
+            </ButtonItem>
           </div>
         );
       })}
     </TabErrorsAccordion>
-  )
-}
+  );
+};
